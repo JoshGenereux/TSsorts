8c79f509600078373117f2562ab50b2f000e47ba
Josh Genereux <jgenereux@stratacache.com>
2022-05-13 11:11:19 -0400

    initial

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('hi');
+const logStuff = () => {
+    console.log('testing');
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('hi')
+
+const logStuff = ()=>{
+  console.log('testing');
+}
